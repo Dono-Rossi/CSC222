fbe9d972273cf46fee2f51464d4c72dbadf37e27
Donnie <donovan_rossi@live.com>
2024-12-17 09:04:36 -0500

    more code

--- ch13/cards.h
+++ ch13/cards.h
@@ -2,6 +2,7 @@
 #include <string>
 using namespace std;
 
+// Structure
 struct Card
 {
     Rank rank;
@@ -9,6 +10,56 @@ struct Card
 
     Card(Suit s, Rank r);
 }
-enum Suit {NONE, CLUBS, DIAMONDS, HEARTS, SPADES};
+
+struct Deck {
+    vector<Card> cards;
+
+    Deck(int n);
+};
+
 enum Rank {JOKER, TWO=2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT,
            NINE, TEN, JACK, QUEEN, KING, ACE};
+// Constructor
+
+Deck::Deck(int size)
+{
+    vector<Card> temp(size);
+    cards = temp;
+}
+
+Deck::Deck()
+{
+    vector<Card> temp(52);
+    cards = temp;
+    int i = 0;
+    for (Suit suit = CLUBS; suit <= SPADES; suit = Suit(suit+1)) {
+        for (Rank rank = ACE; rank <= KING; rank = Rank(rank+1)) {
+            cards[i].suit = suit;
+            cards[i].rank = rank;
+            i++;
+        }
+    }
+}
+
+void Deck::print() const
+{
+    for (int i = 0; i < cards.size(); i++) {
+        cout << cards[i].to_string() << endl;
+    }
+}
+
+int Card::find(const Deck& deck) const
+{
+    for (int i = 0; i < deck.cards.size(); i++) {
+        if (deck.cards[i].equals(*this)) return i;
+    }
+    return -1;
+}
+
+void Deck::shuffle()
+{
+    for (int i = 0; i < cards.size(); i++) {
+        int rand_card = random_between(0, cards.size() - 1);
+        swap_cards(i, rand_card);
+    }
+}
