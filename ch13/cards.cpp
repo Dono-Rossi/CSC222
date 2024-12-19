20ddfad1d83c33fdbf002353367d654eb0d984e3
Donnie <donovan_rossi@live.com>
2024-12-19 09:24:24 -0500

    working on test 2 errors

--- ch13/cards.cpp
+++ ch13/cards.cpp
@@ -22,4 +22,66 @@ std::string Card::to_string() const
     return rank_strings[rank] + " of " + suit_strings[suit];
 }
 
+bool Card::operator==(const Card& c2) const
+{
+    return (rank == c2.rank && suit == c2.suit);
+}
 
+bool Card::operator>(const Card& c2) const
+{
+    // first check the suits
+    if (suit > c2.suit) return true;
+    if (suit < c2.suit) return false;
+
+    // if suits are equal, check ranks
+    if (rank > c2.rank) return true;
+    if (rank < c2.rank) return false;
+    // this last statement can be omitted without changing the
+    // behavior of the function, but making it arguably less readable
+
+    // if ranks are equal too, 1st card is not greater than the 2nd
+    return false;
+}
+
+bool Card::operator>=(const Card& c2) const
+{
+    // first check the suits
+    if (suit > c2.suit) return true;
+    if (suit < c2.suit) return false;
+
+    // if suits are equal, check ranks
+    if (rank > c2.rank) return true;
+    if (rank < c2.rank) return false;
+    // this last statement can be omitted without changing the
+    // behavior of the function, but making it arguably less readable
+
+    // equal ranks
+    return true;
+}
+
+bool Card::operator<(const Card& c2) const
+{
+    if (suit < c2.suit) return true;
+    if (suit > c2.suit) return false;
+
+    if (rank < c2.rank) return true;
+    if (rank > c2.rank) return false;
+    
+    return false;
+}
+
+bool Card::operator=<(const Card& c2) const
+{
+    if (suit < c2.suit) return true;
+    if (suit > c2.suit) return false;
+
+    if (rank < c2.rank) return true;
+    if (rank > c2.rank) return false;
+    
+    return true;
+}
+
+bool Card::operator!=(const Card& c2) const
+{
+    return (rank != c2.rank && suit != c2.suit);
+}
