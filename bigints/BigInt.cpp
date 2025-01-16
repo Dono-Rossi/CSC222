b14949b87626a1d2a29fb6e8b7ca01bbe6e15160
Donnie <donovan_rossi@live.com>
2025-01-16 08:34:00 -0500

    pass test 4

--- bigints/BigInt.cpp
+++ bigints/BigInt.cpp
@@ -39,25 +39,25 @@ bool BigInt::operator>(const BigInt& b2) const{
 bool BigInt::operator<(const BigInt& b2) const{
     int bInt1 = stoi((!negative) ? digits : "-" + digits);
     int bInt2 = stoi((!b2.negative) ? b2.digits : "-" + b2.digits);
-    return(bInt1>bInt2);
+    return(bInt1<bInt2);
 }
 
 bool BigInt::operator>=(const BigInt& b2) const{
     int bInt1 = stoi((!negative) ? digits : "-" + digits);
     int bInt2 = stoi((!b2.negative) ? b2.digits : "-" + b2.digits);
-    return(bInt1>bInt2);
+    return(bInt1>=bInt2);
 }
 
 bool BigInt::operator<=(const BigInt& b2) const{
     int bInt1 = stoi((!negative) ? digits : "-" + digits);
     int bInt2 = stoi((!b2.negative) ? b2.digits : "-" + b2.digits);
-    return(bInt1>bInt2);
+    return(bInt1<=bInt2);
 }
 
 bool BigInt::operator!=(const BigInt& b2) const{
     int bInt1 = stoi((!negative) ? digits : "-" + digits);
     int bInt2 = stoi((!b2.negative) ? b2.digits : "-" + b2.digits);
-    return(bInt1>bInt2);
+    return(bInt1!=bInt2);
 }
 
 
