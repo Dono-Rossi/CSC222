adb5fb17c406ba47cee4a6a6eed7c9ba607a2d7d
Donnie <donovan_rossi@live.com>
2025-01-16 08:25:59 -0500

    pass test 3

--- bigints/BigInt.cpp
+++ bigints/BigInt.cpp
@@ -31,8 +31,8 @@ bool BigInt::operator==(const BigInt& b2) const
 }
 
 bool BigInt::operator>(const BigInt& b2) const{
-int bInt1 = stoi((!negative) ? digits : "-" + digits);
-int bInt2 = stoi((!b2.negative) ? digits : "-" + digits); 
-return 0;
+    int bInt1 = stoi((!negative) ? digits : "-" + digits);
+    int bInt2 = stoi((!b2.negative) ? b2.digits : "-" + b2.digits); 
+    return(bInt1>bInt2);
 }
 
